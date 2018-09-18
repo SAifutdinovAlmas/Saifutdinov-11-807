bcf2ad94d9932a6dc16e54359b23c6867bcc64c0
SAifutdinovAlmas <werwerqsc@mail.ru>
2018-09-18 11:53:55 +0300

    HOMEWORk

--- src/com/company/Aa.java
+++ src/com/company/Aa.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Aa {
+        public static void main(String[] args) {
+            int[] arr = {3, 4, 2, 11, 133};
+            String str = "";
+            for (int i = 0; i < arr.length; i++) {
+                str += arr[i];
+            }
+            int x = Integer.parseInt(str);
+            System.out.println(x);
+        }
 }
