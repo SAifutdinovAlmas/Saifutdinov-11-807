a1c7491e12ff3eb7339be77b4214dc727c8faa31
SAifutdinovAlmas <werwerqsc@mail.ru>
2018-09-18 10:18:35 +0300

    homework

--- src/com/company/rda.java
+++ src/com/company/rda.java
@@ -1,8 +1,21 @@
 package com.company;
 
-public class rda {
-    qwfeasd kernfi
-    wqeirf o
-    asd
-            FORI
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        String arr1 = "", arr2 = "";
+        for (int i = 0; i < m; i++) {
+            arr1 += sc.nextInt();
+        }
+        for (int i = 0; i < k; i++) {
+            arr2 += sc.nextInt();
+        }
+        System.out.println(arr2.indexOf(arr1));
+    }
 }
