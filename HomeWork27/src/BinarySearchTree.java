78a91de0b276791a9739c0da179d47ec8539b1e7
SAifutdinovAlmas <43250141+SAifutdinovAlmas@users.noreply.github.com>
2019-04-29 01:35:30 +0300

    Add files via upload

---
+++ HomeWork27/src/BinarySearchTree.java
@@ -0,0 +1,7 @@
+public interface BinarySearchTree<T extends Comparable> {
+    void insert(T t);
+    void remove(T t);
+    boolean contains(T t);
+    void printAll();
+    void printAllByLevels();
+}
