64e699815e9dd9c85a141738f6c4b51c1fe197fe
SAifutdinovAlmas <43250141+SAifutdinovAlmas@users.noreply.github.com>
2019-05-06 23:13:31 +0300

    Delete BinarySearchTree.java

--- HomeWork27/src/BinarySearchTree.java
+++
@@ -1,7 +0,0 @@
-public interface BinarySearchTree<T extends Comparable> {
-    void insert(T t);
-    void remove(T t);
-    boolean contains(T t);
-    void printAll();
-    void printAllByLevels();
-}
